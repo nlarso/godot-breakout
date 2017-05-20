69c919a683524534987d2f00d6754576d27be110
Nick Larson <nick@nick-larson.net>
2017-05-20 09:00:01 -0600

    Set velocity and direction of the ball when it hits the paddle

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,5 +1,8 @@
 extends RigidBody2D
 
+const SPEEDUP = 400
+const MAXSPEED = 30000
+
 func _ready():
 	set_fixed_process(true)
 	
@@ -8,3 +11,8 @@ func _fixed_process(delta):
 	for body in bodies:
 		if body.is_in_group("Bricks"):
 			body.queue_free()
+		elif body.get_name() == "Paddle":
+			var speed = get_linear_velocity().length()
+			var direction = get_pos() - body.get_node("AnchorPoint").get_global_pos()
+			var velocity = direction.normalized()*min(speed+SPEEDUP*delta, MAXSPEED*delta)
+			set_linear_velocity(velocity)
