1448abf63495d53767a2bdce62cd824968b8702e
Nick Larson <nick@nick-larson.net>
2017-05-19 22:31:19 -0600

    Add basic paddle movement

---
+++ Scripts/paddle.gd
@@ -0,0 +1,9 @@
+extends KinematicBody2D
+
+func _ready():
+	set_fixed_process(true)
+	
+func _fixed_process(delta):
+	var y = get_pos().y
+	var x = get_viewport().get_mouse_pos().x
+	set_pos(Vector2(x, y))
